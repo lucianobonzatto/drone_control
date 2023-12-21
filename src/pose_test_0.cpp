7ff053d0413288e629b5a16d13fa1599e68efc1f
luciano <luciano.bj23@hotmail.com>
2023-12-21 14:13:16 +0000

    pose tests

---
+++ src/pose_test_0.cpp
@@ -0,0 +1,20 @@
+
+#include "../include/drone_control.h"
+#include "../include/ros_client.h"
+
+int main(int argc, char **argv)
+{
+  ROSClient ros_client(argc, argv);
+  DroneControl drone_control(&ros_client);
+
+  drone_control.guidedMode();
+  drone_control.takeOff();
+  int altitude = 5;
+
+  drone_control.hover(5);
+  drone_control.flyToLocal(0, 0, altitude, 0);
+  drone_control.hover(5);
+
+  drone_control.land();
+  return 0;
+}
--- src/pose_test_2.cpp
+++ src/pose_test_2.cpp
@@ -9,15 +9,18 @@ int main(int argc, char **argv)
 
   drone_control.guidedMode();
   drone_control.takeOff();
-  int altitude = 5;
+  int altitude = 6;
 
+  drone_control.hover(5);
   drone_control.flyToLocal(0, 0, altitude, 0);
   drone_control.hover(5);
   drone_control.flyToLocal(2, 0, altitude, 0);
   drone_control.hover(5);
   drone_control.flyToLocal(2, 0, altitude, 0);
   drone_control.hover(5);
-  drone_control.flyToLocal(-4, 0, altitude, 0);
+  drone_control.flyToLocal(-2, 0, altitude, 0);
+  drone_control.hover(5);
+  drone_control.flyToLocal(-2, 0, altitude, 0);
   drone_control.hover(5);
 
   drone_control.land();
